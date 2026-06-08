e7dfea0c0ccc05d082eadd43b701c292922446ef
Dominik Schrempf <dominik.schrempf@gmail.com>
2026-06-08 20:23:32 +0200

    Nix env

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "Blog";
+
+  inputs = {
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs =
+    inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [ ];
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "aarch64-darwin"
+        "x86_64-darwin"
+      ];
+      perSystem =
+        {
+          pkgs,
+          ...
+        }:
+        {
+          devShells.default = pkgs.mkShell {
+            nativeBuildInputs = [
+              pkgs.hugo
+            ];
+          };
+        };
+      flake = { };
+    };
+}
